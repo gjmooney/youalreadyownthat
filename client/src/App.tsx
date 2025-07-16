b8a7641015587b11ca4fbd2ae57dbe1065b8fa70
Greg <gregory.mooney@quantstack.net>
2025-07-16 19:18:49 +0200

    Use neobrutalism components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
       <p>A typesafe fullstack monorepo</p>
       <div className="flex items-center gap-4">
         <Button onClick={sendRequest}>Call API</Button>
-        <Button variant="secondary" asChild>
+        <Button asChild>
           <a target="_blank" href="https://bhvr.dev">
             Docs
           </a>
